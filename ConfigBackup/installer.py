d2b74566db86cec4c30fa44b4d4b86989fcb25e4
Troy Mendoza <mendoza.dev1235813@gmail.com>
2021-06-21 17:24:56 -0700

    Updating methods to improve readability.

--- ConfigBackup/installer.py
+++ ConfigBackup/installer.py
@@ -1,38 +1,34 @@
 import os
+import time
 
 libs = [
     'netmiko',
 ]
 
-not_installed_libs = []
+
+def create_file(cmd):
+    with open('installed_libs.txt', 'w+') as file:
+        os.system('{} > installed_libs.txt'.format(cmd))
 
 
 def check_libs():
-    installed = 'pip list'
-    line_list = []
-    with open('installed_libs.txt', 'w+') as file:
-        os.system(installed + ' > installed_libs.txt')
-        for line in file:
-            line_list.append(line)
-    conjoin = ' '.join(line_list)
-    return conjoin
-
-
-def not_installed():
-    conjoin = check_libs()
+    not_installed_libs = []
     for lib in libs:
-        if lib in conjoin:
-            print(lib + ' is currently installed.')
-        elif lib not in conjoin:
+        if lib in open('installed_libs.txt').read():
+            print('{} is currently installed.'.format(lib))
+        elif lib not in open('installed_libs.txt').read():
             not_installed_libs.append(lib)
+            print(not_installed_libs)
     return not_installed_libs
 
 
 def install_libs():
-    for install in not_installed():
+    for install in check_libs():
         os.system('pip3 install {}'.format(install))
-        os.system('rm installed_libs.txt')
+        print(install)
+    os.system('rm installed_libs.txt')
 
 
 if __name__ == '__main__':
+    create_file('pip list')
     install_libs()
