39215a13ff5706b29ea372d746797696bd4cf88d
Troy Mendoza <mendoza.dev1235813@gmail.com>
2021-06-26 17:33:05 -0700

    Updating README.md to reflect script functionality

--- ConfigBackup/installer.py
+++ ConfigBackup/installer.py
@@ -12,25 +12,27 @@ class InstallLibraries:
     read_file = open('installed_libs.txt', 'r+')
 
 
-    def create_file(cmd):
+    def create_file(cmd, txt):
         with InstallLibraries.write_file:
-            os.system('{} > installed_libs.txt'.format(cmd))
+            os.system('{} > {}'.format(cmd, txt))
 
 
     def check_libs():
         for lib in libs:
-            if lib not in InstallLibraries.read_file.read():
+            if lib in InstallLibraries.read_file.read():
+                print('{} already installed'.format(lib))
+            elif lib not in InstallLibraries.read_file.read():
                 InstallLibraries.not_installed_libs.append(lib)
-                print("Not Installed: {}".format(InstallLibraries.not_installed_libs))
+                print("Installed Libs: {}".format(InstallLibraries.not_installed_libs))
         return InstallLibraries.not_installed_libs
 
 
-    def install_libs():
+    def install_libs(txt):
         for install in InstallLibraries.check_libs():
             os.system('pip3 install {}'.format(install))
-        os.system('rm installed_libs.txt')
+        os.system('rm {}'.format(txt))
 
 
 if __name__ == '__main__':
-    InstallLibraries.create_file('pip list')
-    InstallLibraries.install_libs()
+    InstallLibraries.create_file('pip list', 'installed_libs.txt')
+    InstallLibraries.install_libs('installed_libs.txt')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Networking
-Scripts that are used for automating network tasks.
+
+Config Backup Is a command line utility used to automate the process 
+of backing up the running configurations of network devices.
+
+Update the "network_addresses" list with the management IP addresses 
+for routers, switches and firewalls on the network.
